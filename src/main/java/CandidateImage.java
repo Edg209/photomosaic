312bcb901828ff606d17304cdd85d6f156720092
Ed <Edgrant209@googlemail.com>
2021-11-14 00:19:46 +0000

    Added dummy classes for CandidateImage and TargetImage

---
+++ src/main/java/CandidateImage.java
@@ -0,0 +1,14 @@
+/**
+ * CandidateImage is an object that represents a single candidate image for a photomosaic.
+ * The data on the individual pixels is in CIE-Lab format.
+ *
+ * TODO: Add details of fields
+ */
+public class CandidateImage {
+    public CandidateImage(String path) {
+        //TODO: Read from the path
+        //TODO: Resize to the chosen dimensions
+        //TODO: Apply background colour
+        //TODO: Convert to CIE-Lab
+    }
+}
---
+++ src/main/java/TargetImage.java
@@ -0,0 +1,14 @@
+/**
+ * TargetImage is an object that represents the target image of a photomosaic.
+ * The data on the individual pixels is in CIE-Lab format.
+ *
+ * TODO: Add details of fields
+ */
+public class TargetImage {
+    public TargetImage(String path) {
+        //TODO: Read from the path
+        //TODO: Resize to the chosen dimensions
+        //TODO: Apply background colour
+        //TODO: Convert to CIE-Lab
+    }
+}
